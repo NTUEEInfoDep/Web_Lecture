14d6fa8cc62e8a2027ffec190e87220e4ee40807
byronhsu <byronhsu1230@gmail.com>
2018-11-15 09:42:17 +0800

    server on gh-pages

--- week1/example/tzyLee/BMI.js
+++
@@ -1,9 +0,0 @@
-let temp = window.onload;
-window.onload = function(...args) {
-    temp && temp(...args);
-    let BMICalc = document.getElementById('BMI-Calc');
-    let [_, height, __, weight, button] = BMICalc.children;
-    button.addEventListener('click', () => {
-        alert(`Your BMI is ${+weight.value/(+height.value/100)**2}`);
-    });
-}
