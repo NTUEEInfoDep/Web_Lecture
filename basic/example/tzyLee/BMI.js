101bc4e223578bed91d71ce664026fbbede118e8
tzyLee <b06901145@ntu.edu.tw>
2018-11-16 21:18:54 +0800

    Add a simple progress bar to show the BMI

--- basic/example/tzyLee/BMI.js
+++ basic/example/tzyLee/BMI.js
@@ -2,15 +2,19 @@ let temp = window.onload; // Save the previous window.onload function (Although 
 window.onload = function(...args) {
     temp && temp(...args); // Short circuit evaluation / Coercion, if temp is defined, execute temp, forwarding all arguments
     let BMICalc = document.getElementById('BMI-Calc'); // <section id='BMI-Calc'></section>
-    let [_, height, __, ___, weight, ____, button, result] = BMICalc.children;
+    let [_, height, __, ___, weight, ____, button, result, progressBar] = BMICalc.children;
     /*
         height: <input name='BMI-Calc_Height' type="number" min='0'></input>
         weight: <input name='BMI-Calc_Weight' type="number" min='0'></input>
         button: <input type='button' value='Submit'></input>
         result: <p></p>
+        progressBar: <progress min='0' max='100'></progress>
      */
     button.addEventListener('click', () => {
-        result.textContent = `Your BMI is ${+weight.value/(+height.value/100)**2}`;
+        let BMI = +weight.value/(+height.value/100)**2;
+        result.textContent = `Your BMI is ${BMI}`;
+        if(BMI >= 0 && BMI <= 100)
+            progressBar.value = BMI;
         // <p>textContent</p> => <p>`Your BMI is ${+weight.value/(+height.value/100)**2}`</p>
     });
 }
