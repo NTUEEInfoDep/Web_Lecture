0513e1072a5e2dbfc747973facd7820f321f5625
tzyLee <b06901145@ntu.edu.tw>
2018-11-23 18:52:30 +0800

    Make the example easier

--- Todo-List/todo.js
+++ Todo-List/todo.js
@@ -1,25 +1,27 @@
 window.onload = () => {
-    (() => {
-        class TodoList {
-            static add() {
-                if (TodoList.input.value.trim()) {
-                    let newEntry = document.createElement('li');
-                    newEntry.innerHTML = TodoList.input.value;
-                    newEntry.addEventListener('click', TodoList.remove);
-
-                    TodoList.list.appendChild(newEntry);
-                    TodoList.input.value = '';
-                }
-            }
-
-            static remove(event) {
-                TodoList.list.removeChild(event.target);
+    class TodoList {
+        static add() {
+            if (TodoList.input.value.trim()) {
+                let newEntry = document.createElement('li');
+                let check = document.createElement('input');
+                check.type = "checkbox";
+                newEntry.appendChild(check);
+                console.log(newEntry)
+                newEntry.textContent += TodoList.input.value;
+                newEntry.addEventListener('click', TodoList.remove);
+                
+                TodoList.list.appendChild(newEntry);
+                TodoList.input.value = '';
             }
         }
 
-        TodoList.input = document.getElementById('input');
-        TodoList.button = document.getElementById('add_button');
-        TodoList.list = document.getElementById('todo_list');
-        TodoList.button.addEventListener('click', TodoList.add);
-    })();
+        static remove(event) {
+            TodoList.list.removeChild(event.target);
+        }
+    }
+
+    TodoList.input = document.getElementById('input');
+    TodoList.button = document.getElementById('add_button');
+    TodoList.list = document.getElementById('todo_list');
+    TodoList.button.addEventListener('click', TodoList.add);
 }
